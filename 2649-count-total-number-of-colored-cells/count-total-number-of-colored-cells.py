28de91f41445c9abdceca480b4d1a05a87ec25ee
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-05 15:42:37 +0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.45%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -1,6 +1,7 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        if n == 1: 
-            return 1
-        n -= 1
-        return int(n / 2 * (2 * 4 + (n - 1) * 4) + 1)
+        # 1, 4, 8, 12, ..., an
+        # 1, 4*1, 4*2, 4*3, ..., 4*(n-1)
+        # 1 + 4 * n*(n-1)/2
+        # 1 + 2*n*(n-1)
+        return 1 + 2 * n * (n - 1)

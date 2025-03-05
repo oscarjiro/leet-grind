2f34dd41758e553f73a35054b21c88352ecdf937
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-05 15:45:02 +0700

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,8 +1,16 @@
+#pragma GCC optimize("O2")
+
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
 class Solution 
 {
 public:
     long long coloredCells(int n) 
     {
+        fast_io;
         return 1 + 2 * (long long)n * (n - 1);
     }
 };

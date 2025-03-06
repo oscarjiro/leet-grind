f9e1883b277f9e15297d8ac04b07fe0ec535851f
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-06 10:56:41 +0700

    Time: 2 ms (95.79%) | Memory: 18.4 MB (8.94%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,17 +1,38 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: list[list[int]]) -> list[int]:
-        ans: list[int] = [-1, -1]
-        count_dict: dict[int, int] = {i: 0 for i in range(1, len(grid) * len(grid) + 1)}
+        # Actual sum of all numbers and squared numbers in grid
+        actual_sum: int = 0
+        actual_sqr_sum: int = 0
+        for row in grid:
+            for num in row:
+                actual_sum += num
+                actual_sqr_sum += num ** 2
 
-        for g in grid:
-            for num in g:
-                count_dict[num] += 1
-                if count_dict[num] == 2:
-                    ans[0] = num
+        # Sum of n natural numbers: [n(n+1)]/2
+        # Sum of n squared natural numbers: [n(n+1)(2n+1)]/6
+        n: int = len(grid) ** 2
+        perfect_sum: int = n * (n + 1) / 2
+        perfect_sqr_sum: int = n * (n + 1) * (2 * n + 1) / 6
+
+        # a: repeat occurences, b: missing
+        # actual_sum = perfect_sum + a - b
+        # sum_diff = a - b
+        # actual_sqr_sum = perfect_sqr_sum + a^2 - b^2
+        # sum_sqr_diff = a^2 - b^2
+        # a^2 - b^2 = (a+b)(a-b)
+        # sum_sqr_diff = (a+b) * sum_diff
+
+        # a + b = sum_sqr_diff / sum_diff
+        # a - b = sum_diff
+        # a = [(sum_sqr_diff / sum_diff) + sum_diff]/2
+        # b = [(sum_sqr_diff / sum_diff) - sum_diff]/2
+
+        sum_diff: int = actual_sum - perfect_sum
+        sum_sqr_diff: int = actual_sqr_sum - perfect_sqr_sum
         
-        for num, count in count_dict.items():
-            if count == 0:
-                ans[1] = num
-                break
+        repeat: int = int((sum_sqr_diff / sum_diff + sum_diff) / 2)
+        missing: int = int((sum_sqr_diff / sum_diff - sum_diff) / 2)
         
-        return ans
+        return [repeat, missing]
+
+

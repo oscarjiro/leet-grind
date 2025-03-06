14a1ad3b10e63436cda543f4d4761b135d042db6
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-06 10:59:41 +0700

    Time: 3 ms (94.14%) | Memory: 18.2 MB (63.86%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -11,9 +11,10 @@ class Solution:
         # Sum of n natural numbers: [n(n+1)]/2
         # Sum of n squared natural numbers: [n(n+1)(2n+1)]/6
         n: int = len(grid) ** 2
-        perfect_sum: int = n * (n + 1) / 2
-        perfect_sqr_sum: int = n * (n + 1) * (2 * n + 1) / 6
+        perfect_sum: int = n * (n + 1) // 2
+        perfect_sqr_sum: int = n * (n + 1) * (2 * n + 1) // 6
 
+        # BREAKING DOWN EXPRESSIONS
         # a: repeat occurences, b: missing
         # actual_sum = perfect_sum + a - b
         # sum_diff = a - b
@@ -21,7 +22,8 @@ class Solution:
         # sum_sqr_diff = a^2 - b^2
         # a^2 - b^2 = (a+b)(a-b)
         # sum_sqr_diff = (a+b) * sum_diff
-
+    
+        # SUB & ELIMINATION TO FIND REPEAT & MISSING
         # a + b = sum_sqr_diff / sum_diff
         # a - b = sum_diff
         # a = [(sum_sqr_diff / sum_diff) + sum_diff]/2
@@ -30,8 +32,8 @@ class Solution:
         sum_diff: int = actual_sum - perfect_sum
         sum_sqr_diff: int = actual_sqr_sum - perfect_sqr_sum
         
-        repeat: int = int((sum_sqr_diff / sum_diff + sum_diff) / 2)
-        missing: int = int((sum_sqr_diff / sum_diff - sum_diff) / 2)
+        repeat: int = (sum_sqr_diff // sum_diff + sum_diff) // 2
+        missing: int = (sum_sqr_diff // sum_diff - sum_diff) // 2
         
         return [repeat, missing]
 

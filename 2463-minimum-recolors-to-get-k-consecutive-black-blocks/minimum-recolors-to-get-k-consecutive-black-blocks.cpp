9dfd5583c4b41a056f90a074b7539ea0785ff209
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-08 16:45:59 +0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.01%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,8 +1,17 @@
+#pragma GCC optimize("O2")
+
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
 class Solution 
 {
 public:
     int minimumRecolors(string blocks, int k) 
     {
+        fast_io;
+        
         int min_recolors = 0;
         for (int i = 0; i < k; ++i)
             if (blocks[i] == 'W')

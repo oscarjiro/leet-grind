77cb51373fde484789b9f4ebdeb7f19101d672ae
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-08 16:28:44 +0700

    Time: 1431 ms (59.42%) | Memory: 35.2 MB (18.21%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -11,6 +11,9 @@ class Solution:
         return primes
 
     def closestPrimes(self, left: int, right: int) -> list[int]:
+        if left == 2 and right == 3:
+            return [left, right]
+
         all_primes: list[bool] = self.sieve(right)
         primes: list[bool] = [i for i in range(left, right + 1) if all_primes[i]]
 
@@ -18,9 +21,14 @@ class Solution:
             return [-1, -1]
 
         ans: list[int] = [primes[0], primes[1]]
+        min_delta: int = primes[1] - primes[0]
+
         for i in range(1, len(primes) - 1):
-            if primes[i + 1] - primes[i] < ans[1] - ans[0]:
+            delta: int = primes[i + 1] - primes[i]
+            if delta < min_delta:
                 ans[0] = primes[i]
                 ans[1] = primes[i + 1]
+                if delta == 2:
+                    break
 
         return ans

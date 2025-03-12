b50813aecc739c850664915251aba2a4f9d411bf
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-12 23:50:23 +0700

    Time: 0 ms (100.00%) | Memory: 21.4 MB (43.55%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,8 +1,16 @@
+#pragma GCC optimize("O2")
+
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
 class Solution 
 {
 public:
     int maximumCount(vector<int>& nums) 
     {
+        fast_io;
         int neg = 0, nol = 0;
         for (auto num : nums)
             if (num < 0)

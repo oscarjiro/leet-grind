6504228fe5f2c16c91047a50c8a741eec836dcd2
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-17 16:25:54 +0700

    Time: 4 ms (14.93%) | Memory: 8.8 MB (64.18%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.c
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.c
@@ -1,12 +1,19 @@
+int compare(const void* a, const void* b) 
+{
+    return (*(int*)a - *(int*)b);  
+}
+
 bool divideArray(int* nums, int numsSize) 
 {
-    int freq[501] = {}, min_num = 500, max_num = 1;
-    for (int i = 0; i < numsSize; ++i)
-        ++freq[nums[i]], 
-        min_num = fmin(min_num, nums[i]), 
-        max_num = fmax(max_num, nums[i]);
-    for (int i = min_num; i <= max_num; ++i)
-        if (freq[i] % 2 != 0)
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    for (int i = 0; i < numsSize;)
+    {
+        int freq = 0, current = nums[i];
+        while (i < numsSize && nums[i] == current)
+            ++freq, ++i;
+        if (freq % 2 != 0)
             return false;
+    }
     return true;
 }

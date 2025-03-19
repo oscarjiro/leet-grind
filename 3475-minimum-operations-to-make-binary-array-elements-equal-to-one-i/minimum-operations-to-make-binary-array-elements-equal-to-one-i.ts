f331b1ed4ba2a7e3044d1f4e7b93734e43cb6228
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-19 13:46:19 +0700

    Time: 6 ms (84.62%) | Memory: 65.6 MB (46.15%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.ts
@@ -0,0 +1,10 @@
+function minOperations(nums: number[]): number {
+    let n = 0;
+    for (let i = 0; i < nums.length - 2; ++i)
+        if (!nums[i]) 
+            ++n,
+            nums[i] = 1,
+            nums[i + 1] ^= 1,
+            nums[i + 2] ^= 1;
+    return nums.at(-1) && nums.at(-2) ? n : -1;
+};

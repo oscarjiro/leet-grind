1b9756aafac3b314d429a0cae27429dff0652163
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-21 17:18:21 +0700

    Time: 694 ms (7.72%) | Memory: 98.9 MB (99.31%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -4,10 +4,14 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) 
     {
         unordered_set<string> available(supplies.begin(), supplies.end());
+
+        int used_count = 0;
         vector<bool> used(recipes.size(), false);
         vector<string> valid_recipes;
 
-        for (int i = 0; i < recipes.size(); ++i)
+        for (int i = 0; used_count < recipes.size() && i < recipes.size(); ++i)
+        {
+            bool has_diverged = true;
             for (int j = 0; j < recipes.size(); ++j)
             {
                 if (used[j])
@@ -21,8 +25,13 @@ public:
                 if (k == ingredients[j].size())
                     valid_recipes.push_back(recipes[j]),
                     available.insert(recipes[j]),
-                    used[j] = true;
+                    used[j] = true,
+                    ++used_count,
+                    has_diverged = false;
             }
+            if (has_diverged)
+                break;
+        }
 
         return valid_recipes;
     }

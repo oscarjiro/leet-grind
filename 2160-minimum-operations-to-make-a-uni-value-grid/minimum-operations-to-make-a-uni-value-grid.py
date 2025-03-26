be57c447987fb442e16025ba778068e4f1657e1a
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-26 10:26:05 +0700

    Time: 123 ms (91.23%) | Memory: 39.6 MB (19.30%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minOperations(self, grid: list[list[int]], x: int) -> int:
+        if (n := len(grid) * len(grid[0])) == 1:
+            return 0
+
+        arr: list[int] = []
+        mod: int = -1
+        for row in grid:
+            for num in row:
+                arr.append(num)
+                if mod == -1:
+                    mod = num % x
+                elif num % x != mod:
+                    return -1
+        
+        arr = sorted(arr)
+        median: int = arr[n // 2 - (n % 2 == 0)]
+
+        operations = 0
+        for row in grid:
+            for num in row:
+                operations += abs(num - median) // x
+        
+        return operations

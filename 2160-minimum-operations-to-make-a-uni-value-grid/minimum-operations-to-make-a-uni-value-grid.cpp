21659caa3a85bd54401e53719c1290e7c09b8730
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-26 10:34:07 +0700

    Time: 42 ms (57.75%) | Memory: 86.7 MB (81.76%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -5,8 +5,7 @@
     cin.tie(0);                   \
     cout.tie(0);
 
-class Solution 
-{
+class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) 
     {
@@ -16,30 +15,37 @@ public:
         if (n == 1)
             return 0;
 
-        int remainder = -1;
-        vector<int> sorted;
+        vector<int> nums;
         for (const auto &row : grid)
             for (int num : row)
             {
-                sorted.push_back(num);
-                if (remainder == -1)
-                    remainder = num % x;
-                else if (num % x != remainder)
+                if (num % x != grid[0][0] % x)
                     return -1;
+                nums.push_back(num);
             }
+        sort(nums.begin(), nums.end());
 
-        sort(sorted.begin(), sorted.end());
-        int median = sorted[n / 2 - (n % 2 == 0)];
-
-        int operations = 0;
-        for (const auto &row : grid)
-            for (int num : row)
+        int operations = 0, prefix_index = 0, suffix_index = n - 1;
+        while (prefix_index < suffix_index) 
+            if (prefix_index < n - suffix_index - 1) 
             {
-                if (num == median)
-                    continue;
-                operations += abs(num - median) / x;
+                int prefix_operations =
+                    (prefix_index + 1) *
+                    (nums[prefix_index + 1] - nums[prefix_index]) / x;
+
+                operations += prefix_operations;
+                ++prefix_index;
+            } 
+            else 
+            {
+                int suffix_operations =
+                    (n - suffix_index) *
+                    (nums[suffix_index] - nums[suffix_index - 1]) / x;
+
+                operations += suffix_operations;
+                --suffix_index;
             }
-        
+
         return operations;
     }
 };

3a73c13eb0f88dfbd01ec9e784c83a446fe24b57
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-03-26 10:35:09 +0700

    Time: 37 ms (71.12%) | Memory: 86.7 MB (81.76%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -5,7 +5,8 @@
     cin.tie(0);                   \
     cout.tie(0);
 
-class Solution {
+class Solution 
+{
 public:
     int minOperations(vector<vector<int>>& grid, int x) 
     {
@@ -15,37 +16,27 @@ public:
         if (n == 1)
             return 0;
 
-        vector<int> nums;
+        vector<int> sorted;
         for (const auto &row : grid)
             for (int num : row)
             {
                 if (num % x != grid[0][0] % x)
                     return -1;
-                nums.push_back(num);
-            }
-        sort(nums.begin(), nums.end());
-
-        int operations = 0, prefix_index = 0, suffix_index = n - 1;
-        while (prefix_index < suffix_index) 
-            if (prefix_index < n - suffix_index - 1) 
-            {
-                int prefix_operations =
-                    (prefix_index + 1) *
-                    (nums[prefix_index + 1] - nums[prefix_index]) / x;
-
-                operations += prefix_operations;
-                ++prefix_index;
-            } 
-            else 
-            {
-                int suffix_operations =
-                    (n - suffix_index) *
-                    (nums[suffix_index] - nums[suffix_index - 1]) / x;
-
-                operations += suffix_operations;
-                --suffix_index;
+                sorted.push_back(num);
             }
 
+        sort(sorted.begin(), sorted.end());
+        int median = sorted[n / 2 - (n % 2 == 0)];
+
+        int operations = 0;
+        for (const auto &row : grid)
+            for (int num : row)
+            {
+                if (num == median)
+                    continue;
+                operations += abs(num - median) / x;
+            }
+        
         return operations;
     }
 };

6146c2c4136c5136b36abf22d9c5895d6ba9d156
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-04-02 08:22:41 +0700

    Time: 3 ms (31.33%) | Memory: 21.9 MB (24.40%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,8 +1,16 @@
+#pragma GCC optimize("O2")
+
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
 class Solution 
 {
 public:
     long long maximumTripletValue(vector<int>& nums) 
     {
+        fast_io;
         long long max_value = 0;
         for (int i = 0; i < nums.size() - 2; ++i)
             for (int j = i + 1; j < nums.size() - 1; ++j)

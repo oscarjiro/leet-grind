c559010188d217935c7f129f1faa62781b8e2b30
Oscar Jiro <40124362+oscarjiro@users.noreply.github.com>
2025-04-18 15:05:29 +0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (96.47%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,8 +1,17 @@
+#pragma GCC optimize("O2")
+
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
 class Solution 
 {
 public:
     string countAndSay(int n) 
     {
+        fast_io;
+
         string s = "1";
         for (int i = 2; i <= n; ++i)
         {
